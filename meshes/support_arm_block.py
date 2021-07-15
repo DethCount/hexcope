3648d7e9965ed8f09b040d670cb7a66d16b4552d
Dethcount <count.msn@gmail.com>
2021-07-15 03:43:07 +0200

    support arm block

--- meshes/support_arm_block.py
+++ meshes/support_arm_block.py
@@ -31,6 +31,8 @@ def create_mesh(
         x0 + m,
         math.sqrt((arm_points[0][0] + m) ** 2 + h ** 2)
     )
+    max_angle = math.asin(h / outer_r)
+    x3 = outer_r * math.cos(max_angle)
 
     if n % 2 == 0:
         y2 = hex2xyz(f, r, math.floor(0.5 * n), 1, 5, 1)[2]
@@ -62,61 +64,93 @@ def create_mesh(
     h4 = min(v2[2], v3[2], v4[2])
     h5 = max(v2[2], v3[2], v4[2]) + h3
 
+    ar = arm_radius + 0.5 * e
+
     vertices = [
-        (v0[0], v0[1], v0[2]),
+        (v0[0], v0[1], h4),
         (v0[0], v0[1], v0[2] + h0),
         (v0[0], v0[1], v0[2] + h1),
-        (v0[0], v0[1], v0[2] + h3),
+        (v0[0], v0[1], h5),
 
-        (v1[0], v1[1], v1[2]),
+        (v1[0], v1[1], h4),
         (v1[0], v1[1], v1[2] + h0),
         (v1[0], v1[1], v1[2] + h1),
-        (v1[0], v1[1], v1[2] + h3),
+        (v1[0], v1[1], h5),
 
         # 8
-        (v2[0], v2[1], v2[2]),
+        (v2[0], v2[1], h4),
         (v2[0], v2[1], v2[2] + h0),
         (v2[0], v2[1], v2[2] + h1),
-        (v2[0], v2[1], v2[2] + h3),
+        (v2[0], v2[1], h5),
 
-        (v3[0], v3[1], v3[2]),
+        (v3[0], v3[1], h4),
         (v3[0], v3[1], v3[2] + h0),
         (v3[0], v3[1], v3[2] + h1),
-        (v3[0], v3[1], v3[2] + h3),
+        (v3[0], v3[1], h5),
 
         # 16
-        (v4[0], v4[1], v4[2]),
+        (v4[0], v4[1], h4),
         (v4[0], v4[1], v4[2] + h0),
         (v4[0], v4[1], v4[2] + h1),
-        (v4[0], v4[1], v4[2] + h3),
+        (v4[0], v4[1], h5),
 
         # 20
-        (v0[0] - 2 * hex_thickness, v0[1], v0[2]),
+        (v0[0] - 2 * hex_thickness, v0[1], h4),
         (v0[0] - 2 * hex_thickness, v0[1], v0[2] + h0),
         (v0[0] - 2 * hex_thickness, v0[1], v0[2] + h1),
-        (v0[0] - 2 * hex_thickness, v0[1], v0[2] + h3),
+        (v0[0] - 2 * hex_thickness, v0[1], h5),
 
-        (v1[0] - 2 * hex_thickness, v1[1], v1[2]),
+        (v1[0] - 2 * hex_thickness, v1[1], h4),
         (v1[0] - 2 * hex_thickness, v1[1], v1[2] + h0),
         (v1[0] - 2 * hex_thickness, v1[1], v1[2] + h1),
-        (v1[0] - 2 * hex_thickness, v1[1], v1[2] + h3),
+        (v1[0] - 2 * hex_thickness, v1[1], h5),
 
         # 28
-        (v2[0] - 2 * hex_thickness, v2[1], v2[2]),
+        (v2[0] - 2 * hex_thickness, v2[1], h4),
         (v2[0] - 2 * hex_thickness, v2[1], v2[2] + h0),
         (v2[0] - 2 * hex_thickness, v2[1], v2[2] + h1),
-        (v2[0] - 2 * hex_thickness, v2[1], v2[2] + h3),
+        (v2[0] - 2 * hex_thickness, v2[1], h5),
 
-        (v3[0] - 2 * hex_thickness, v3[1], v3[2]),
+        (v3[0] - 2 * hex_thickness, v3[1], h4),
         (v3[0] - 2 * hex_thickness, v3[1], v3[2] + h0),
         (v3[0] - 2 * hex_thickness, v3[1], v3[2] + h1),
-        (v3[0] - 2 * hex_thickness, v3[1], v3[2] + h3),
+        (v3[0] - 2 * hex_thickness, v3[1], h5),
 
         # 36
-        (v4[0] - 2 * hex_thickness, v4[1], v4[2]),
+        (v4[0] - 2 * hex_thickness, v4[1], h4),
         (v4[0] - 2 * hex_thickness, v4[1], v4[2] + h0),
         (v4[0] - 2 * hex_thickness, v4[1], v4[2] + h1),
-        (v4[0] - 2 * hex_thickness, v4[1], v4[2] + h3),
+        (v4[0] - 2 * hex_thickness, v4[1], h5),
+
+        # 40
+        (arm_points[0][0] - ar, arm_points[0][1] - ar, h4),
+        (arm_points[0][0] - ar, arm_points[0][1] - ar, h5),
+
+        (arm_points[0][0] - ar, arm_points[0][1] + ar, h4),
+        (arm_points[0][0] - ar, arm_points[0][1] + ar, h5),
+
+        (arm_points[0][0] + ar, arm_points[0][1] - ar, h4),
+        (arm_points[0][0] + ar, arm_points[0][1] - ar, h5),
+
+        (arm_points[0][0] + ar, arm_points[0][1] + ar, h4),
+        (arm_points[0][0] + ar, arm_points[0][1] + ar, h5),
+
+        # 48
+        (arm_points[1][0] - ar, arm_points[1][1] - ar, h4),
+        (arm_points[1][0] - ar, arm_points[1][1] - ar, h5),
+
+        (arm_points[1][0] - ar, arm_points[1][1] + ar, h4),
+        (arm_points[1][0] - ar, arm_points[1][1] + ar, h5),
+
+        (arm_points[1][0] + ar, arm_points[1][1] - ar, h4),
+        (arm_points[1][0] + ar, arm_points[1][1] - ar, h5),
+
+        (arm_points[1][0] + ar, arm_points[1][1] + ar, h4),
+        (arm_points[1][0] + ar, arm_points[1][1] + ar, h5),
+
+        # 56
+        (x3, 0, h4),
+        (x3, 0, h5),
     ]
     edges = [
         (0, 3),
@@ -145,18 +179,27 @@ def create_mesh(
         (20, 21, 25, 24), (24, 25, 29, 28), (28, 29, 33, 32), (32, 33, 37, 36),
         (1, 2, 6, 5), (5, 6, 10, 9), (9, 10, 14, 13), (13, 14, 18, 17),
         (22, 23, 27, 26), (26, 27, 31, 30), (30, 31, 35, 34), (34, 35, 39, 38),
-        (0, 1, 21, 20), (16, 17, 37, 36),
-        (2, 3, 23, 22), (18, 19, 39, 38),
-        (0, 4, 24, 20), (4, 8, 28, 24), (8, 12, 32, 28), (12, 16, 36, 32),
+        (0, 1, 21, 20), (17, 16, 36, 37),
+        (2, 3, 23, 22), (19, 18, 38, 39),
+        (4, 0, 20, 24), (8, 4, 24, 28), (12, 8, 28, 32), (16, 12, 32, 36),
         (1, 5, 25, 21), (5, 9, 29, 25), (9, 13, 33, 29), (13, 17, 37, 33),
-        (2, 6, 26, 22), (6, 10, 30, 26), (10, 14, 34, 30), (14, 18, 38, 34),
+        (6, 2, 22, 26), (10, 6, 26, 30), (14, 10, 30, 34), (18, 14, 34, 38),
         (3, 7, 27, 23), (7, 11, 31, 27), (11, 15, 35, 31), (15, 19, 39, 35),
+
+        (0, 4, 42, 40),
+        (7, 3, 41, 43),
+
+        (12, 16, 50, 48),
+        (19, 15, 49, 51),
+
+        (4, 8, 42),
+        (11, 7, 43),
+
+        (8, 12, 48),
+        (15, 11, 49),
     ]
 
     nb_verts = len(vertices)
-
-    max_angle = math.asin(h / outer_r)
-    print('max_angle', str(max_angle), str(h / outer_r))
 
     for i in range(0, p + 1):
         alpha = i * max_angle / p
@@ -166,7 +209,6 @@ def create_mesh(
         beta = i * math.tau / p
         cb = math.cos(beta)
         sb = math.sin(beta)
-        ar = arm_radius + 0.5 * e
 
         vertices.extend([
             (outer_r * ca, outer_r * sa, h5),
@@ -214,7 +256,40 @@ def create_mesh(
                 (ltv - nbidx, ltv, lbv, lbv - nbidx),
                 (lctv - nbidx, lctv, lcbv, lcbv - nbidx),
                 (rctv - nbidx, rctv, rcbv, rcbv - nbidx),
+                (rtv - nbidx, rtv, 57),
+                (rbv, rbv - nbidx, 56),
+                (ltv, ltv - nbidx, 57),
+                (lbv - nbidx, lbv, 56),
             ])
+
+            if beta < math.pi / 2:
+                faces.extend([
+                    (rctv, rctv - nbidx, 55),
+                    (rcbv - nbidx, rcbv, 54),
+                    (lctv, lctv - nbidx, 47),
+                    (lcbv - nbidx, lcbv, 46),
+                ])
+            elif beta < math.pi:
+                faces.extend([
+                    (rctv, rctv - nbidx, 51),
+                    (rcbv - nbidx, rcbv, 50),
+                    (lctv, lctv - nbidx, 43),
+                    (lcbv - nbidx, lcbv, 42),
+                ])
+            elif beta < 3 * math.pi / 2:
+                faces.extend([
+                    (rctv, rctv - nbidx, 49),
+                    (rcbv - nbidx, rcbv, 48),
+                    (lctv, lctv - nbidx, 41),
+                    (lcbv - nbidx, lcbv, 40),
+                ])
+            else:
+                faces.extend([
+                    (rctv, rctv - nbidx, 53),
+                    (rcbv - nbidx, rcbv, 52),
+                    (lctv, lctv - nbidx, 45),
+                    (lcbv - nbidx, lcbv, 44),
+                ])
 
     edges.extend([
         (rtv, 19),
@@ -225,8 +300,40 @@ def create_mesh(
 
     faces.extend([
         (3, 0, lbv, ltv),
-        (19, 16, rbv, rtv),
+        (16, 19, rtv, rbv),
+
+        (ltv, 45, 41, 3),
+        (44, lbv, 0, 40),
+
+        (55, rtv, 19, 51),
+        (rbv, 54, 50, 16),
     ])
+
+    if n % 2 == 0:
+        faces.extend([
+            (ltv, 45, 47),
+            (lbv, 44, 46),
+            (rtv, 53, 55),
+            (rbv, 52, 54),
+            (ltv, rtv, 53, 47),
+            (rbv, lbv, 46, 52),
+            (47, 53, 49, 43),
+            (52, 46, 42, 48),
+            (43, 49, 11),
+            (42, 8, 48),
+        ])
+    else:
+        faces.extend([
+            (45, ltv, 11, 47),
+            (rtv, 55, 53, 11),
+            (lbv, 44, 46, 8),
+            (54, rbv, 8, 52),
+
+            (47, 11, 43),
+            (46, 42, 8),
+            (53, 49, 11),
+            (52, 8, 48),
+        ])
 
     mesh.from_pydata(vertices, edges, faces)
     mesh.update()
